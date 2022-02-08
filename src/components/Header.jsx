7ed13dbc31e61be45879cf91610bac5d459a8e0e
Anatoly Marchuk <70931679+AnatoliyMarchuk@users.noreply.github.com>
2022-02-08 07:33:05 +0200

    Network Start

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Header() {
+	return (
+		<header className="header">
+			<img
+				className="logo"
+				src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Logo_TV_2015.svg/1200px-Logo_TV_2015.svg.png"
+				alt="img"
+			/>
+		</header>
+	);
+}
