a16f53245d9c2d1ad56ce58826683a757334c335
Anatoliy Marchuk <anatoliymarchuk87@gmail.com>
2022-02-18 11:22:15 +0200

    add react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import Header from './components/Header';
-import Navbar from './components/Navbar';
+import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile';
 import './App.css';
-
+import { Routes, Route } from 'react-router-dom';
 function App() {
 	return (
 		<div className="app-wrapper">
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import s from './Navbar.module.css';
 
 export default function Navbar() {
 	return (
